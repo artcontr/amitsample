09f477306d9a719a882979cf62cde24dce94d780
artcontr <artcontr@gmail.com>
2021-02-23 17:38:26 +0200

    Initial commit

---
+++ handler.js
@@ -0,0 +1,82 @@
+(function () {
+  const btn1 = document.querySelector('#btn1');
+  const btn2 = document.querySelector('#btn2');
+  const btn3 = document.querySelector('#btn3');
+
+  let ws;
+
+  function handleReq(data) {
+    data = JSON.parse(data);
+    switch (data.type) {
+      case 'changeTime':
+        changeTime(data.value);
+        break;
+      case 'changeCount':
+        changeCount(data.value);
+        break;
+      case 'changeWave':
+        changeWave(data.value);
+        break;
+    }
+    console.log(data.type)
+  }
+
+  function init() {
+    if (ws) {
+      ws.onerror = ws.onopen = ws.onclose = null;
+      ws.close();
+    }
+
+    ws = new WebSocket('ws://localhost:8080');
+    ws.onopen = () => {
+      console.log('Connection opened!');
+    }
+    ws.onmessage = ({ data }) => handleReq(data);
+    ws.onclose = function () {
+      ws = null;
+    }
+  }
+  if (btn1) {
+    btn1.onclick = function () {
+      if (!ws) {
+        showMessage("No WebSocket connection :(");
+        return;
+      }
+
+      ws.send(JSON.stringify({
+        type: 'changeTime',
+        value: 5
+      }));
+
+    }
+  }
+  if (btn2) {
+    btn2.onclick = function () {
+      if (!ws) {
+        showMessage("No WebSocket connection :(");
+        return;
+      }
+
+      ws.send(JSON.stringify({
+        type: 'changeCount',
+        value: 222
+      }));
+
+    }
+  }
+  if (btn3) {
+    btn3.onclick = function () {
+      if (!ws) {
+        showMessage("No WebSocket connection :(");
+        return;
+      }
+
+      ws.send(JSON.stringify({
+        type: 'changeWave',
+        value: 50
+      }));
+
+    }
+  }
+  init();
+})();
---
+++ script.js
@@ -0,0 +1,129 @@
+let pixelCount = 144;
+let canvasWidth = 144 * 4;
+let canvasHeight = 10;
+let pixelWidth = Math.round(canvasWidth / pixelCount);
+let pixelHeight = canvasHeight;
+
+let timing=0.07;
+let waveCount=2;
+
+let currentPixel = 0;
+let currentContext;
+
+function interval(ms) {
+  
+}
+
+function time(interval) {
+  return (performance.now() % (65536 * interval)) / (65536 * interval);
+}
+
+function wave(v) {
+  return (1 + Math.sin(v * Math.PI * 2)) / 2;
+}
+
+// todo: add more waveform functions
+// todo: add more render functions
+
+function hsv(h, s, v) {
+  currentContext.fillStyle = hsvToRgb(h, s, v);
+  currentContext.fillRect(
+    pixelWidth * currentPixel,
+    0,
+    pixelWidth,
+    pixelHeight
+  );
+}
+
+function hsvToRgb(h, s, v) {
+  h = h % 1;
+  h = h * 6;
+  let i = Math.floor(h),
+    f = h - i,
+    p = v * (1 - s),
+    q = v * (1 - f * s),
+    t = v * (1 - (1 - f) * s),
+    mod = i % 6,
+    r = [v, q, p, p, t, v][mod],
+    g = [t, v, v, q, p, p][mod],
+    b = [p, p, t, v, v, q][mod];
+
+  return `rgb(${(r * 255) | 0}, ${(g * 255) | 0}, ${(b * 255) | 0})`;
+}
+
+function addStrip(beforeRenderFn, renderFn) {
+  const strip = document.createElement("canvas");
+  document.body.appendChild(strip);
+  strip.className = "canvas";
+  strip.setAttribute("width", canvasWidth + "px");
+  strip.setAttribute("height", canvasHeight + "px");
+
+  const ctx = strip.getContext("2d");
+  
+  // Recompile from source to escape strict mode and let them reference
+  // undeclared variables, and add Math to the top of the scope chain.
+  // This lets us avoid redeclaring all the math functions/constants.
+  beforeRenderFn = Function(
+    "with (Math) { return ( " + beforeRenderFn + ")() }"
+  );
+  renderFn = Function(
+    ["index"],
+    "with (Math) { return (" + renderFn + ")(index) }"
+  );
+
+  function update() {
+    currentContext = ctx;
+    beforeRenderFn(); // todo: add 'delta'
+    for (currentPixel = 0; currentPixel < pixelCount; currentPixel++) {
+      renderFn(currentPixel);
+    }
+
+    requestAnimationFrame(update);
+  }
+
+  update();
+}
+
+
+
+function init() {
+  // Add a bunch of strips!
+ let canv= document.querySelectorAll("canvas");
+  if(canv.length>0){
+    canv.forEach(element => {
+      element.remove();
+    });
+  }
+  for (let i = 0; i < waveCount; i++) {
+    addStrip(
+      delta => {
+        hl = pixelCount / 2;
+        t = time(0.1);
+        v = 0.01 + wave(time(timing));
+      },
+      index => {
+        c1 = 1 - abs(index - hl) / hl;
+        hsv(c1 + t, 1, v);
+      }
+    );
+    
+   
+  }
+}
+function changeTime(value){
+  console.log(value);
+  timing=value;
+  init();
+}
+function changeCount(value){
+  console.log(value);
+  pixelCount=value;
+  init();
+}
+function changeWave(value){
+  console.log(value);
+  waveCount=value;
+  init();
+}
+
+init();
